ee34378bcbf803a4d9983e3ad035984c07d8aff3
Роман Лещенко <xetyri2014@gmail.com>
2022-11-05 15:08:56 +0200

    add

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import { Routes, Route } from "react-router-dom";
+import { Home } from "./views/Home";
+import { About } from "./views/About";
+import { BrowserRouter } from "react-router-dom";
+
+function App() {
+    return (
+        <BrowserRouter >
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+            </Routes>
+        </BrowserRouter>
+    )
+}
+
+export default App;
